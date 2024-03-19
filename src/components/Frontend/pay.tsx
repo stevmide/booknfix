5282b6648cc0a2c06f64d81b852f3ff7c87cb436
stevmide <olu.ojumoola@gmail.com>
2024-03-19 09:16:48 +0000

    refactor

--- src/components/Frontend/pay.tsx
+++ src/components/Frontend/pay.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { PaystackButton } from 'react-paystack'
+// import React from 'react';
+// import { PaystackButton } from 'react-paystack'
 
-const PayButton = ({ amount, email }) => {
-  const publicKey = process.env.sk_test_fa6f49f790ec4676ce96b3bfb82b9696f1203581;
-  const [reference, setReference] = React.useState('');
+// const PayButton = ({ amount, email }) => {
+//   const publicKey = process.env.sk_test_fa6f49f790ec4676ce96b3bfb82b9696f1203581;
+//   const [reference, setReference] = React.useState('');
 
-  const handlePaystackSuccessAction = (reference) => {
-    // handle payment success
-  }
+//   const handlePaystackSuccessAction = (reference) => {
+//     // handle payment success
+//   }
 
-  const componentProps = {
-    email,
-    amount,
-    publicKey,
-    text: 'Pay Now',
-    onSuccess: (reference) => handlePaystackSuccessAction(reference),
-    onClose: () => alert('Payment canceled by user.'),
-  };
+//   const componentProps = {
+//     email,
+//     amount,
+//     publicKey,
+//     text: 'Pay Now',
+//     onSuccess: (reference) => handlePaystackSuccessAction(reference),
+//     onClose: () => alert('Payment canceled by user.'),
+//   };
 
-  return <PaystackButton {...componentProps} />;
+//   return <PaystackButton {...componentProps} />;
   
 
-};
+// };
 
-export default PayButton;
+// export default PayButton;
