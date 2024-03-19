45ff4faef82764805793dd2197138ab4707ef872
stevmide <olu.ojumoola@gmail.com>
2024-03-19 09:19:02 +0000

    refactor

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Layout from "@/components/Frontend/layout"
-import Pay from "@/components/Frontend/pay"
+// import React from 'react'
+// import Layout from "@/components/Frontend/layout"
+// import Pay from "@/components/Frontend/pay"
 
 
-function payment() {
-  return (
-    <Layout>
-    <div className="mx-auto max-w-screen-xl">
-      <Pay />
-    </div>
-  </Layout>
-  )
-}
+// function payment() {
+//   return (
+//     <Layout>
+//     <div className="mx-auto max-w-screen-xl">
+//       <Pay />
+//     </div>
+//   </Layout>
+//   )
+// }
 
-export default payment
+// export default payment
